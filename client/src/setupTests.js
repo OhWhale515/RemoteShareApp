1b5c49cbf1d8a92dcf608d4e49d261d3565ebb75
OhWhale515 <sterlinggreen515@gmail.com>
2023-06-14 17:33:42 -0400

    client side updates

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,16 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  const { queryByText } = render(<App />);
+  const linkElement = queryByText(/learn react/i);
+
+  // Assert that the element is either found or null
+  expect(linkElement).toBeInTheDocument();
+
+  if (linkElement) {
+    // Additional assertions when the element is found
+    expect(linkElement).toHaveTextContent(/learn react/i);
+    // Add more assertions if needed
+  }
+});
