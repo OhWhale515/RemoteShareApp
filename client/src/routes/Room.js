9231d1b58042b27626fa6fc1d2a727c5d5be7e07
OhWhale515 <sterlinggreen515@gmail.com>
2023-06-14 21:46:04 -0400

    room vid logic

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -10,96 +10,95 @@ const Room = (props) => {
 
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-  .then((stream) => {
-  userVideo.current.srcObject = stream;
-  userStream.current = stream;
-  socketRef.current = io.connect("/"); // Provide the appropriate URL to connect to the server
+      .then((stream) => {
+        userVideo.current.srcObject = stream;
+        userStream.current = stream;
+        socketRef.current = io.connect("/"); // Provide the appropriate URL to connect to the server
 
-  socketRef.current.emit("join room", props.roomID); // Replace "props.roomID" with the actual room ID
+        socketRef.current.emit("join room", props.roomID); // Replace "props.roomID" with the actual room ID
 
-  socketRef.current.on("other user", () => {
-    // The other user is connected, start the call
-    socketRef.current.emit("user joined", props.roomID); // Replace "props.roomID" with the actual room ID
-  });
+        socketRef.current.on("other user", () => {
+          // The other user is connected, start the call
+          socketRef.current.emit("user joined", props.roomID); // Replace "props.roomID" with the actual room ID
+        });
 
-  socketRef.current.on("user joined", () => {
-    // The call has been accepted by the other user, start the communication
-    socketRef.current.emit("start communication", props.roomID); // Replace "props.roomID" with the actual room ID
-  });
+        socketRef.current.on("user joined", () => {
+          // The call has been accepted by the other user, start the communication
+          socketRef.current.emit("start communication", props.roomID); // Replace "props.roomID" with the actual room ID
+        });
 
-  socketRef.current.on("offer", (offer) => {
-    // Receive the offer from the other user and set the remote description
-    socketRef.current.emit("answer", { type: "answer", sdp: offer });
-  });
+        socketRef.current.on("offer", (offer) => {
+          // Receive the offer from the other user and set the remote description
+          socketRef.current.emit("answer", { type: "answer", sdp: offer });
+        });
 
-  socketRef.current.on("answer", (answer) => {
-    // Receive the answer from the other user and set the remote description
-    userPeer.current.setRemoteDescription(answer);
-  });
+        socketRef.current.on("answer", (answer) => {
+          // Receive the answer from the other user and set the remote description
+          userPeer.current.setRemoteDescription(answer);
+        });
 
-  socketRef.current.on("ice-candidate", (candidate) => {
-    // Receive ICE candidate from the other user and add it to the peer connection
-    userPeer.current.addIceCandidate(candidate);
-  });
+        socketRef.current.on("ice-candidate", (candidate) => {
+          // Receive ICE candidate from the other user and add it to the peer connection
+          userPeer.current.addIceCandidate(candidate);
+        });
 
-  const userPeer = new RTCPeerConnection(); // Create a new RTCPeerConnection
+        const userPeer = new RTCPeerConnection(); // Create a new RTCPeerConnection
 
-  userStream.current.getTracks().forEach((track) => {   
-    socketRef.current.emit("start screen share", props.roomID, stream); // Replace "props.roomID" with the actual room ID
+        userStream.current.getTracks().forEach((track) => {
+          // Add all tracks from the user's stream to the peer connection
+          userPeer.current.addTrack(track, userStream.current);
+        });
 
-    setScreenShareActive(true);
-  })
-  .catch((error) => {
-    console.error("Error accessing screen share:", error);
-  });
+        userPeer.current.ontrack = (event) => {
+          // Set the remote stream as the source for the partner's video element
+          partnerVideo.current.srcObject = event.streams[0];
+        };
 
-    // Add all tracks from the user's stream to the peer connection
-    userPeer.current.addTrack(track, userStream.current);
-  });
+        userPeer.current.onicecandidate = (event) => {
+          // Send ICE candidate to the other user
+          if (event.candidate) {
+            socketRef.current.emit("ice-candidate", event.candidate);
+          }
+        };
 
-  userPeer.current.ontrack = (event) => {
-    // Set the remote stream as the source for the partner's video element
-    partnerVideo.current.srcObject = event.streams[0];
-  };
-
-  userPeer.current.onicecandidate = (event) => {
-    // Send ICE candidate to the other user
-    if (event.candidate) {
-      socketRef.current.emit("ice-candidate", event.candidate);
-    }
-  };
-
-  socketRef.current.on("start communication", () => {
-    // Start the communication by creating and sending an offer to the other user
-    userPeer.current.createOffer().then((offer) => {
-      socketRef.current.emit("offer", offer.sdp);
-      userPeer.current.setLocalDescription(offer);
-    });
-  });
-})
-.catch((error) => {
-  console.error("Error accessing media devices:", error);
-});
-
-    
-
+        socketRef.current.on("start communication", () => {
+          // Start the communication by creating and sending an offer to the other user
+          userPeer.current.createOffer().then((offer) => {
+            socketRef.current.emit("offer", offer.sdp);
+            userPeer.current.setLocalDescription(offer);
+          });
+        });
+      })
+      .catch((error) => {
+        console.error("Error accessing media devices:", error);
+      });
   }, []);
 
   function startScreenShare() {
-    // Add code to start the screen sharing
-    setScreenShareActive(true);
+    navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
+      .then((stream) => {
+        userVideo.current.srcObject = stream;
+        userStream.current = stream;
+
+        // Replace the code below with the appropriate logic for sending the screen share stream to the other user
+        socketRef.current.emit("start screen share", props.roomID, stream); // Replace "props.roomID" with the actual room ID
+
+        setScreenShareActive(true);
+      })
+      .catch((error) => {
+        console.error("Error accessing screen share:", error);
+      });
   }
 
   function stopScreenShare() {
     userStream.current.getTracks().forEach((track) => {
-    track.stop();
+      track.stop();
     });
+
     userVideo.current.srcObject = userStream.current;
 
-
+    // Replace the code below with the appropriate logic for stopping the screen share on the other user's side
     socketRef.current.emit("stop screen share", props.roomID); // Replace "props.roomID" with the actual room ID
-
-setScreenShareActive(false);
 
     setScreenShareActive(false);
   }
